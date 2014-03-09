1879a5e61aece16714b2bc393c9ba49eb3229ab8
Hays Clark <hays@infinitedescent.com>
2014-03-09 15:54:26 -0700

    adding some basic lighting and altering the startup command to use the new reversi piece

--- Assets/scripts/reversi/game/controller/GameStartupCommand.cs
+++ Assets/scripts/reversi/game/controller/GameStartupCommand.cs
@@ -16,14 +16,30 @@ namespace reversi.game
 
 		override public void Execute()
 		{
-			GameObject diskPrefab = resources.Load<GameObject>("Prefabs/Disk");
+			addSceneLighting();
+			demoPopulatedScene();
+		}
 
+		void addSceneLighting ()
+		{
+			GameObject lightGameObject = new GameObject("Key Light");
+			lightGameObject.AddComponent<Light>();
+			lightGameObject.light.type = LightType.Directional;
+			lightGameObject.transform.Rotate ( new Vector3( -30f, -30f, 0) );
+			lightGameObject.light.color = Color.white;
+			lightGameObject.light.intensity = 0.4f;
+		}
+
+		void demoPopulatedScene ()
+		{
+			GameObject diskPrefab = resources.Load<GameObject>("Prefabs/reversi_piece");
 			for (int row = 0; row < 4; row++)
 			{
 				for (int col = 0; col < 4; col++)
 				{
 					GameObject cur = (GameObject)gameObject.Instantiate(diskPrefab);
-					cur.transform.position = new Vector3(1f * col, 1f * row, 10f);
+					cur.transform.position = new Vector3( 1f * col, 1f * row, 0f);
+					cur.transform.Rotate( Vector3.right, 90 );
 				}
 			}
 		}
