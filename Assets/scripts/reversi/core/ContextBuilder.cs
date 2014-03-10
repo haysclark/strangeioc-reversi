60fbc6a86e2b3ecf3c9106a6df9b3fedb4b6a388
Hays Clark <hays@infinitedescent.com>
2014-03-09 23:39:38 -0700

    Moving all Builder classes back into main builder class to keep  things together.

--- Assets/scripts/reversi/core/ContextBuilder.cs
+++ Assets/scripts/reversi/core/ContextBuilder.cs
@@ -134,3 +134,18 @@ public class MapperActionListExecuter
 		_mapContextList.Clear();
 	}
 }
+
+public class FirstRunOnly
+{
+	public static Action<MVCSContext> Do( Action<MVCSContext> action )
+	{
+		Action<MVCSContext> wrapper = delegate( MVCSContext context )
+		{
+			if( Context.firstContext == context )
+			{
+				action( context );
+			}
+		};
+		return wrapper;
+	}
+}
--- Assets/scripts/reversi/game/GameBootstrap.cs
+++ Assets/scripts/reversi/game/GameBootstrap.cs
@@ -35,6 +35,5 @@ namespace reversi.game
 		{
 			context.commandBinder.Bind<CreateGridSpacesSignal> ().To<CreateGridSpacesCommand> ();
 		}
-
 	}
 }
