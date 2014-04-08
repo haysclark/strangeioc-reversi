690e891bd535fa56e593d97d4b7847f79b486cb0
wlindley <walker@walkerlindley.com>
2014-04-07 21:09:11 -0500

    Implementing MoveRuleFactory.

--- Assets/scripts/reversi/game/model/MoveRuleFactory.cs
+++ Assets/scripts/reversi/game/model/MoveRuleFactory.cs
@@ -1,13 +1,25 @@
 using System;
 using System.Collections.Generic;
+using strange.extensions.injector.api;
 
 namespace reversi.game
 {
 	public class MoveRuleFactory : IMoveRuleFactory
 	{
+		private List<IMoveRule> rules = new List<IMoveRule>();
+
+		[Inject]
+		public IInjectionBinder InjectionBinder { get; set; }
+
+		[PostConstruct]
+		public void PostInject()
+		{
+			rules.Add(InjectionBinder.GetInstance<HorizontalMoveRule>());
+		}
+
 		public List<IMoveRule> BuildMoveRules()
 		{
-			throw new NotImplementedException();
+			return rules;
 		}
 	}
 }
