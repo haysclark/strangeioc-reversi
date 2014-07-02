87458d4da07086779643d0af355cc8573244c0e7
wlindley <walker@walkerlindley.com>
2014-07-02 14:30:00 -0700

    Refactoring PlacePieceCommand to be easy to understand
    Following Uncle Bob's advice of extract 'til you drop.

--- Assets/scripts/reversi/game/controller/PlacePieceCommand.cs
+++ Assets/scripts/reversi/game/controller/PlacePieceCommand.cs
@@ -6,11 +6,14 @@ namespace reversi.game
 {
 	public class PlacePieceCommand : Command
 	{
-		public GridCellKey position;
-		public Faction faction;
+		[Inject]
+		public GridCellKey position { get; set; }
+		[Inject]
+		public Faction faction { get; set; }
 
 		[Inject]
 		public IMoveRuleFactory MoveRuleFactory { get; set; }
+
 		[Inject]
 		public Grid Grid { get; set; }
 
@@ -19,17 +22,7 @@ namespace reversi.game
 			Faction prevFaction = Grid.GetPiece(position.row, position.col);
 			Grid.PlacePiece(position.row, position.col, faction);
 			List<IMove> moves = FindMoves();
-			if (0 >= moves.Count)
-			{
-				Grid.PlacePiece(position.row, position.col, prevFaction);
-			}
-			else
-			{
-				foreach (var move in moves)
-				{
-					move.ApplyMove(Grid);
-				}
-			}
+			ResolveMoves(moves, prevFaction);
 		}
 
 		private List<IMove> FindMoves()
@@ -37,11 +30,31 @@ namespace reversi.game
 			List<IMove> moves = new List<IMove>();
 			List<IMoveRule> rules = MoveRuleFactory.BuildMoveRules();
 
-			foreach (var rule in rules)
-			{
+			foreach (var rule in rules) {
 				moves.AddRange(rule.FindMoves(position, faction, Grid));
 			}
 			return moves;
+		}
+
+		private void ResolveMoves(List<IMove> moves, Faction prevFaction)
+		{
+			if (0 >= moves.Count) {
+				UndoPlacement(prevFaction);
+			} else {
+				ApplyMoves(moves);
+			}
+		}
+
+		void UndoPlacement(Faction prevFaction)
+		{
+			Grid.PlacePiece(position.row, position.col, prevFaction);
+		}
+
+		void ApplyMoves(List<IMove> moves)
+		{
+			foreach (var move in moves) {
+				move.ApplyMove(Grid);
+			}
 		}
 	}
 }
